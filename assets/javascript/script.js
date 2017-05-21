77b72dc6b00bb109e4aa98a83974148adc78f05b
Melissa <melissarenaut@gmail.com>
2017-05-21 19:04:26 -0400

    initial html and css files

---
+++ assets/javascript/script.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+console.log("hello");
+
+
+var wins = 0;
+var losses = 0;
+var computerScore = 0;
+var userScore = 0;
+
+//start game button, that disappears after game starts
+
+//computer generates a score
+
+//a random number is generated for each crystal
+
+//crystal buttons pushed  and adds up until >= to computer score
+
+//a loss or win is determined and displayed in the final score section
+
+//start button pops up again to begin new game
+
+
+//$("").click();
+
+
+//hide start button when game begins
+		$('h1').hide();
+
+//for (var i = 0; i < letters.length; i++){  
+
+
+
