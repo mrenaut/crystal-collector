e4fdf434ed45fa96933538c7521de832a9daed83
Melissa <melissarenaut@gmail.com>
2017-05-22 20:36:38 -0400

    functional javascript added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,50 @@
 
 
-
-var wins = 0;
-var losses = 0;
-var computerGuess= 0;
-var userScoreVar = 0;
-var blueCrystalVar = 0;
-var yellowCrystalVar = 0;
-var purpleCrystalVar = 0;
-var redCrystalVar = 0;
-
-
-
-//start game button, that disappears after game starts
 $(document).ready(function(){
-		    $("#startButton").click(function(){
+
+	//global variables
+	var wins = 0;
+	var losses = 0;
+	var computerGuess = 0;
+	var userScoreVar = 0;
+	var blueCrystalVar = 0;
+	var yellowCrystalVar = 0;
+	var purpleCrystalVar = 0;
+	var redCrystalVar = 0;
+
+		function scoreCheck() {
+		 if (userScoreVar == computerGuess) {
+		      wins ++;	
+		      console.log("wins: " + wins)	
+		      $("#winLoseMessage").html("You Won!")	
+		      $("#wins").html("Wins: " + wins)  
+		      $("#startButton").show();
+		      $("#userScore").html("<h1>User Score: " + 
+	        userScoreVar + "</h1>");
+		      userScoreVar = 0;
+		
+		    } 
+
+		else if (userScoreVar > computerGuess){
+		      losses ++;	
+		      console.log("losses: " + losses)
+		      $("#winLoseMessage").html("You Lost!")
+		      $("#losses").html("Losses: " + losses) 		      
+		      $("#startButton").show();
+		      $("#userScore").html("<h1>User Score: " + 
+	        userScoreVar + "</h1>");
+		      userScoreVar = 0;
+		    
+			}};
+
+				
+
+		//start game button, that disappears after game starts
+		    $("#startButton").click(function(){	
 	        $(this).hide();
+	        $("#winLoseMessage").html("Totals")
+	        $("#userScore").html("<h1>User Score: " + 
+	        userScoreVar + "</h1>");
 	        //computer generates a random score	up to 40
 	        computerGuess = Math.floor((Math.random() * 40) + 1);
 	        $("#computerScore").html("<h1>Computer Score: " + 
@@ -28,66 +57,66 @@ $(document).ready(function(){
 	        purpleCrystalVar = Math.floor((Math.random() * 12) + 1);
 	        console.log(purpleCrystalVar);
 	        redCrystalVar = Math.floor((Math.random() * 12) + 1);
-	        console.log(redCrystalVar);        	        	
-	    });
+	        console.log(redCrystalVar);  
+
+	        //end of start button function
+	});
+
 
 	    //each time the user clicks a crystal, the score is added
 	   	//to their total count
-
 	   	$("#blueCrystal").click(function(){
 	   		userScoreVar = userScoreVar + blueCrystalVar 
 	   		$("#userScore").html("<h1>User Score: " + 
 	        userScoreVar + "</h1>");
+	        scoreCheck(); 
+
+	             	        
 	    }); 
 
         $("#yellowCrystal").click(function(){
 	   		userScoreVar = userScoreVar + yellowCrystalVar 
 	   		$("#userScore").html("<h1>User Score: " + 
-	        userScoreVar + "</h1>");    
+	        userScoreVar + "</h1>"); 
+	        scoreCheck();	
+	                
 		});
 
 		$("#purpleCrystal").click(function(){
 	   		userScoreVar = userScoreVar + purpleCrystalVar 
 	   		$("#userScore").html("<h1>User Score: " + 
-	        userScoreVar + "</h1>");    
+	        userScoreVar + "</h1>");
+	        scoreCheck(); 	
+	              
 		});
 
 		$("#redCrystal").click(function(){
 	   		userScoreVar = userScoreVar + redCrystalVar 
 	   		$("#userScore").html("<h1>User Score: " + 
-	        userScoreVar + "</h1>");  
+	        userScoreVar + "</h1>");
+	        scoreCheck();  
+	           
 		});
 
-		//if (userScoreVar < 0) {
-		//	console.log(working);
-		//}
-	
-
-});
-
-	
-
-
-
-
-
-
-
-
-//a random number is generated for each crystal
-
-//crystal buttons pushed  and adds up until >= to computer score
+   	
 
 //a loss or win is determined and displayed in the final score section
+		 
+//end of document ready function		    
+});
 
-//start button pops up again to begin new game
+	    //start button pops up again to begin new game
 
 
-//hide start button when game begins
 
-		
 
-//for (var i = 0; i < letters.length; i++){  
+
+
+
+
+
+
+
 
 
 
