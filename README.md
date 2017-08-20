6977fcae217fd029df7c05ce29ec8c062b994b98
Melissa <melissarenaut@gmail.com>
2017-08-19 20:19:37 -0400

    updated pictures and readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# week-4-game
+CRYSTAL COLLECTOR GAME
+===
+
+Basic JavaScript game with random number generator.
+---
+
+
+
+**INSTRUCTIONS**
+
+
+The computer's score is generated when start is clicked.
+
+There are four crystals below.  Clicking on a crystal will add a specific amount of points to your total score.
+
+The game is won by matching your total score to the random number selected by the computer. The game is lost if the total score goes above the computer score.
+
+The value of each crystal is hidden until it is clicked.
+
+Each time when the game starts, the game will change the values of each crystal.
+
+SCREENSHOT FROM GAME
+<!--[rick and morty background with trivia instructions](/images/screenshot1.jpg)-->
+
+![](/assets/images/screenshot1.JPG)
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,4 @@
-
-
-$(document).ready(function(){
+$(document).ready(function () {
 
 	//global variables
 	var wins = 0;
@@ -13,99 +11,81 @@ $(document).ready(function(){
 	var redCrystalVar = 0;
 
 
-		//if win/lose scorechecking function
-		function scoreCheck() {
-		 if (userScoreVar == computerGuess) {
-		      wins ++;	
-		      console.log("wins: " + wins)	
-		      $("#winLoseMessage").html("You Won!")	
-		      $("#wins").html("Wins: " + wins)  
-		      $("#startButton").show();
-		      $("#userScore").html("<h1>User Score: " + 
-	        userScoreVar + "</h1>");
-		      userScoreVar = 0;		
-		    } 
+	//if win/lose scorechecking function
+	function scoreCheck() {
+		if (userScoreVar == computerGuess) {
+			wins++;
+			console.log("wins: " + wins)
+			$("#winLoseMessage").html("You Won!")
+			$("#wins").html("Wins: " + wins)
+			$("#startButton").show();
+			$("#userScore").html("<h1>User Score: " +
+				userScoreVar + "</h1>");
+			userScoreVar = 0;
+		} else if (userScoreVar > computerGuess) {
+			losses++;
+			console.log("losses: " + losses)
+			$("#winLoseMessage").html("You Lost!")
+			$("#losses").html("Losses: " + losses)
+			$("#startButton").show();
+			$("#userScore").html("<h1>User Score: " +
+				userScoreVar + "</h1>");
+			userScoreVar = 0;
+		}
+	};
 
-		else if (userScoreVar > computerGuess){
-		      losses ++;	
-		      console.log("losses: " + losses)
-		      $("#winLoseMessage").html("You Lost!")
-		      $("#losses").html("Losses: " + losses) 		      
-		      $("#startButton").show();
-		      $("#userScore").html("<h1>User Score: " + 
-	        userScoreVar + "</h1>");
-		      userScoreVar = 0;		    
-			}};
+	//start game button
+	$("#startButton").click(function () {
+		$("#winLoseMessage").html("Totals")
+		$("#userScore").html("<h1>User Score: " +
+			userScoreVar + "</h1>");
 
-		//start game button
-		    $("#startButton").click(function(){	
-		        $(this).hide();
-		        $("#winLoseMessage").html("Totals")
-		        $("#userScore").html("<h1>User Score: " + 
-		        userScoreVar + "</h1>");
-		       
-		        //computer generates a random score	up to 40
-		        computerGuess = Math.floor((Math.random() * 101) + 19);
-		        $("#computerScore").html("<h1>Computer Score: " + 
-		        computerGuess + "</h1>");
+		//computer generates a random score	up to 40
+		computerGuess = Math.floor((Math.random() * 101) + 19);
+		$("#computerScore").html("<h1>Computer Score: " +
+			computerGuess + "</h1>");
 
-		        //a score is generated for each crystal up to 12
-		        blueCrystalVar = Math.floor((Math.random() * 12) + 1);
-		        console.log(blueCrystalVar);
-		        yellowCrystalVar = Math.floor((Math.random() * 12) + 1);
-		        console.log(yellowCrystalVar);
-		        purpleCrystalVar = Math.floor((Math.random() * 12) + 1);
-		        console.log(purpleCrystalVar);
-		        redCrystalVar = Math.floor((Math.random() * 12) + 1);
-		        console.log(redCrystalVar); 
-			//end of start button function
-			});
+		//a score is generated for each crystal up to 12
+		blueCrystalVar = Math.floor((Math.random() * 12) + 1);
+		console.log(blueCrystalVar);
+		yellowCrystalVar = Math.floor((Math.random() * 12) + 1);
+		console.log(yellowCrystalVar);
+		purpleCrystalVar = Math.floor((Math.random() * 12) + 1);
+		console.log(purpleCrystalVar);
+		redCrystalVar = Math.floor((Math.random() * 12) + 1);
+		console.log(redCrystalVar);
+		//end of start button function
+	});
 
-	    //each time the user clicks a crystal, the score is added
-	   	//to their total count then checked against the computer score
-	   	$("#blueCrystal").click(function(){
-	   		userScoreVar = userScoreVar + blueCrystalVar 
-	   		$("#userScore").html("<h1>User Score: " + 
-	        userScoreVar + "</h1>");
-	        scoreCheck();	        	                   	        
-	    }); 
+	//each time the user clicks a crystal, the score is added
+	//to their total count then checked against the computer score
+	$("#blueCrystal").click(function () {
+		userScoreVar = userScoreVar + blueCrystalVar
+		$("#userScore").html("<h1>User Score: " +
+			userScoreVar + "</h1>");
+		scoreCheck();
+	});
 
-        $("#yellowCrystal").click(function(){
-	   		userScoreVar = userScoreVar + yellowCrystalVar 
-	   		$("#userScore").html("<h1>User Score: " + 
-	        userScoreVar + "</h1>"); 
-	        scoreCheck();		                
-		});
+	$("#yellowCrystal").click(function () {
+		userScoreVar = userScoreVar + yellowCrystalVar
+		$("#userScore").html("<h1>User Score: " +
+			userScoreVar + "</h1>");
+		scoreCheck();
+	});
 
-		$("#purpleCrystal").click(function(){
-	   		userScoreVar = userScoreVar + purpleCrystalVar 
-	   		$("#userScore").html("<h1>User Score: " + 
-	        userScoreVar + "</h1>");
-	        scoreCheck(); 		              
-		});
+	$("#purpleCrystal").click(function () {
+		userScoreVar = userScoreVar + purpleCrystalVar
+		$("#userScore").html("<h1>User Score: " +
+			userScoreVar + "</h1>");
+		scoreCheck();
+	});
 
-		$("#redCrystal").click(function(){
-	   		userScoreVar = userScoreVar + redCrystalVar 
-	   		$("#userScore").html("<h1>User Score: " + 
-	        userScoreVar + "</h1>");
-	        scoreCheck();	           
-		});
-	 
-//end of document ready function		    
+	$("#redCrystal").click(function () {
+		userScoreVar = userScoreVar + redCrystalVar
+		$("#userScore").html("<h1>User Score: " +
+			userScoreVar + "</h1>");
+		scoreCheck();
+	});
+
+	//end of document ready function		    
 });
-
-	   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
